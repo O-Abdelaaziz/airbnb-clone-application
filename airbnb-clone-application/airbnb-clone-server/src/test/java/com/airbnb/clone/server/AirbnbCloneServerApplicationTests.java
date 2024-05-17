c9427f85ba8a0ae646b2d71c6a1b6ac11eb6b123
aadenfr <aadenfr@gmail.com>
2024-05-17 15:35:05 +0200

    Task 7: create airbnb-clone-server & setup database and docker properties

---
+++ airbnb-clone-application/airbnb-clone-server/src/test/java/com/airbnb/clone/server/AirbnbCloneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbnb.clone.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
